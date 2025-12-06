e02f9cfe420a280809cba9a1d51cfa3d8ddff15c
jeremyxtmeng <xiangtaom@gmail.com>
2025-12-05 19:23:20 -0800

    initial commit

---
+++ test.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.write("Hello world")
+
+# .venv\Scripts\Activate.ps1
+# if not working, then running: Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+# reploy by running: python -m streamlit run test.py
+# close the Edge browser and the terminal to end the session and deactivate to leave the environment.
