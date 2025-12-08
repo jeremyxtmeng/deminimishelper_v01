113683a1095979302eb1791870b3f1d278e2bfbc
jeremyxtmeng <xiangtaom@gmail.com>
2025-12-08 15:07:28 -0800

    delete files in the repo

--- test.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.write("Hello world")
-
-# .venv\Scripts\Activate.ps1
-# if not working, then running: Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-# reploy by running: python -m streamlit run test.py
-# close the Edge browser and the terminal to end the session and deactivate to leave the environment.
