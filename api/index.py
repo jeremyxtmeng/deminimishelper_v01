ebfc7b666cb449a14c0c2e4265a28aaec2668e91
jeremyxtmeng <xiangtaom@gmail.com>
2025-12-18 06:10:04 -0800

    XGboost package is too large

--- api/index.py
+++ api/index.py
@@ -21,11 +21,7 @@ from datetime import datetime, timezone, timedelta
 from supabase import create_client, Client
 from io import BytesIO
 
-# for forecasting
-import statsmodels.formula.api as smf
-from statsforecast import StatsForecast
-from statsforecast.models import AutoARIMA
-from xgboost import XGBRegressor
+
 #-----------------------------------------------------------------
 # Configure Gemini
 #-----------------------------------------------------------------
